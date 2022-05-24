8fc517f3f6f87c330cec294d30d0543f7f6a2b64
lucasdavid <lucasolivdavid@gmail.com>
2022-05-24 10:59:42 -0300

    Improve names, add typing

--- resnet38d.py
+++ resnet38d.py
@@ -1,8 +1,13 @@
+from typing import Union, Optional, Tuple
+
 import tensorflow as tf
 from keras import Model
 from keras.layers import (Activation, Add, BatchNormalization, Conv2D, Dropout,
                           GlobalAveragePooling2D, GlobalMaxPooling2D)
 from keras.utils import data_utils
+from keras.utils.layer_utils import get_source_inputs
+
+S = Union[int, Tuple[int, int]]
 
 ALLOWED_POOLING = ('avg', 'max', None)
 
@@ -21,19 +26,19 @@ WEIGHTS = {
 
 
 def block(
-  x,
-  filters=256,
-  mid_filters=None,
-  kernel_size=3,
-  dilation=1,
-  mid_dilation=None,
-  strides=1,
-  padding='same',
-  use_bias=False,
-  kernel_regularizer=None,
-  kernel_initializer='he_normal',
+  x: tf.Tensor,
+  filters: int = 256,
+  mid_filters: Optional[int] = None,
+  kernel_size: S = 3,
+  dilation: S = 1,
+  mid_dilation: Optional[S] = None,
+  strides: S = 1,
+  padding: str = 'same',
+  use_bias: bool = False,
+  kernel_regularizer: Optional[str] = None,
+  kernel_initializer: Optional[str] = 'he_normal',
   activation: str = 'relu',
-  name: str = None,
+  name: Optional[str] = None,
 ):
   cvargs = dict(kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer, use_bias=use_bias)
 
@@ -57,18 +62,18 @@ def block(
 
 
 def bottleneck(
-  x,
-  filters=256,
-  kernel_size=3,
-  dilation=1,
-  strides=1,
-  padding='same',
-  use_bias=False,
-  kernel_regularizer=None,
-  kernel_initializer='he_normal',
+  x: tf.Tensor,
+  filters: int = 256,
+  kernel_size: S = 3,
+  dilation: S = 1,
+  strides: S = 1,
+  padding: str = 'same',
+  use_bias: bool = False,
+  kernel_regularizer: Optional[str] = None,
+  kernel_initializer: str = 'he_normal',
   activation: str = 'relu',
   dropout_rate: float = 0.,
-  name: str = None,
+  name: Optional[str] = None,
 ):
   cvargs = dict(kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer, use_bias=use_bias)
 
@@ -80,12 +85,12 @@ def bottleneck(
   b2 = Conv2D(filters // 4, kernel_size=1, strides=strides, name=f'{name}/b2/cv', **cvargs)(b2)
   b2 = BatchNormalization(name=f'{name}/b2/bn')(b2)
   b2 = Activation(activation, name=f'{name}/b2/ac')(b2)
-  b2 = Dropout(dropout_rate, name=f'{name}/b2/drop')(b2)
+  b2 = Dropout(dropout_rate, name=f'{name}/b2/dr')(b2)
 
   b3 = Conv2D(filters // 2, kernel_size=kernel_size, strides=strides, dilation_rate=dilation, padding=padding, name=f'{name}/b3/cv', **cvargs)(b2)
   b3 = BatchNormalization(name=f'{name}/b3/bn')(b3)
   b3 = Activation(activation, name=f'{name}/b3/ac')(b3)
-  b3 = Dropout(dropout_rate, name=f'{name}/b3/drop')(b3)
+  b3 = Dropout(dropout_rate, name=f'{name}/b3/dr')(b3)
 
   b4 = Conv2D(filters, kernel_size=1, padding=padding, name=f'{name}/b4/cv', **cvargs)(b3)
 
@@ -93,59 +98,56 @@ def bottleneck(
 
 
 def ResNet38d(
-  input_tensor=None,
-  input_shape=None,
-  classes=20,
-  include_top=True,
-  weights: str = "imagenet",
+  input_tensor: Optional[Union[tf.Tensor, tf.keras.Input]] = None,
+  input_shape: Tuple[int, int, int] = (448, 448, 3),
+  classes: int = 20,
+  include_top: int = True,
+  weights: str = 'voc2012',
   dropout_rate: float = 0,
-  dilated: bool = False,
-  dilation: int = (2, 4),
-  name: str = 'resnet38d',
+  dilation: Optional[S] = (2, 4),
   pooling: str = 'avg',
+  activation: str = 'sigmoid',
+  name: str = 'resnet38d',
 ):
   if pooling not in ALLOWED_POOLING:
     raise ValueError('Illegal value for the `pooling` param. '
                      f'Allowed are {ALLOWED_POOLING}')
 
-  if dilated:
-    dilation = (2, 4)
-  if isinstance(dilation, int) and dilation != 1:
+  if isinstance(dilation, int) and dilation == 1:
     dilation = (dilation // 2, dilation)
-  if isinstance(dilation, (list, tuple)):
-    if len(dilation) != 2:
-      raise ValueError(
-        'Illegal value for the `dilation` param. Must be an int or a pair '
-        '`(mid, end)` containing the mid and end dilation rates.')
+  if not isinstance(dilation, (list, tuple)) or len(dilation) != 2:
+    raise ValueError(
+      f'Illegal value for the `dilation` param "{dilation}". Must be an int '
+      'or a pair `(mid, end)` containing the mid and end dilation rates.')
 
   if input_tensor is None:
     input_tensor = tf.keras.layers.Input(shape=input_shape)
   elif not tf.keras.backend.is_keras_tensor(input_tensor):
-    input_tensor = tf.keras.layers.Input(tensor=input_tensor, shape=input_shape)
+    input_tensor = tf.keras.layers.Input(tensor=input_tensor)
 
-  x = Conv2D(filters=64, kernel_size=3, padding='same', use_bias=False, name='s0/block0/cv')(input_tensor)
+  x = Conv2D(filters=64, kernel_size=3, padding='same', use_bias=False, name='s0/g0/cv')(input_tensor)
 
-  x = block(x, 128, name='s1/block0', strides=2)
-  x = block(x, 128, name='s1/block1')
-  x = block(x, 128, name='s1/block2')
+  x = block(x, 128, name='s1/g0', strides=2)
+  x = block(x, 128, name='s1/g1')
+  x = block(x, 128, name='s1/g2')
 
-  x = block(x, 256, name='s2/block0', strides=2)
-  x = block(x, 256, name='s2/block1')
-  x = block(x, 256, name='s2/block2')
+  x = block(x, 256, name='s2/g0', strides=2)
+  x = block(x, 256, name='s2/g1')
+  x = block(x, 256, name='s2/g2')
 
-  x = block(x, 512, name='s3/block0', strides=2)
-  x = block(x, 512, name='s3/block1')
-  x = block(x, 512, name='s3/block2')
-  x = block(x, 512, name='s3/block3')
-  x = block(x, 512, name='s3/block4')
-  x = block(x, 512, name='s3/block5')
+  x = block(x, 512, name='s3/g0', strides=2)
+  x = block(x, 512, name='s3/g1')
+  x = block(x, 512, name='s3/g2')
+  x = block(x, 512, name='s3/g3')
+  x = block(x, 512, name='s3/g4')
+  x = block(x, 512, name='s3/g5')
 
-  x = block(x, 1024, 512, name='s4/block0', dilation=dilation[0], strides=1, mid_dilation=1)
-  x = block(x, 1024, 512, name='s4/block1', dilation=dilation[0])
-  x = block(x, 1024, 512, name='s4/block2', dilation=dilation[0])
+  x = block(x, 1024, 512, name='s4/g0', dilation=dilation[0], strides=1, mid_dilation=1)
+  x = block(x, 1024, 512, name='s4/g1', dilation=dilation[0])
+  x = block(x, 1024, 512, name='s4/g2', dilation=dilation[0])
 
-  x = bottleneck(x, 2048, name='s5/bottleneck0', dilation=dilation[1], dropout_rate=dropout_rate, strides=1)
-  x = bottleneck(x, 4096, name='s5/bottleneck1', dilation=dilation[1], dropout_rate=dropout_rate)
+  x = bottleneck(x, 2048, name='s5/b0', dilation=dilation[1], dropout_rate=dropout_rate, strides=1)
+  x = bottleneck(x, 4096, name='s5/b1', dilation=dilation[1], dropout_rate=dropout_rate)
   x = BatchNormalization(name='s5/bn')(x)
   x = Activation('relu', name='s5/ac')(x)
 
@@ -154,11 +156,14 @@ def ResNet38d(
 
   if pooling:
     if pooling == 'avg':
-      x = GlobalAveragePooling2D(name='predictions')(x)
+      x = GlobalAveragePooling2D(name='avg_pool')(x)
     else:
-      x = GlobalMaxPooling2D(name='predictions')(x)
+      x = GlobalMaxPooling2D(name='max_pool')(x)
 
-  model = Model(input_tensor, x, name=name)
+  if activation:
+    x = Activation(activation, name='predictions')(x)
+
+  model = Model(get_source_inputs(input_tensor), x, name=name)
 
   if weights is not None:
     if weights not in WEIGHTS:
